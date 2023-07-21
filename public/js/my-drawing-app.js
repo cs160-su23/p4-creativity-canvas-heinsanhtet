b7bd6bb7a14866ba5983147fa24a05fe9b6e5637
Hein San <hein.sanhtet@berkeley.edu>
2023-07-20 18:38:56 -0700

    Handing off to Alex

--- public/js/my-drawing-app.js
+++ public/js/my-drawing-app.js
@@ -1,30 +1,75 @@
-/* The code for our drawing application! 
-Feel free to delete any/all of it and replace with your own functionality. */
-
-var path;
-var currentColor = 'black'
-var currentWidth = 5
-
-tool.onMouseDown = function(event) { //This code in this function is called whenever the mouse is clicked.
-    path = new Path();     // Create a new path each time.
-    path.add(event.point);
-    path.strokeColor = currentColor;
-    path.strokeWidth = currentWidth;
-    console.log(event.point); //this commands log to the Console the coordinates of the mouse click. Feel free to delete it! 
-}
-tool.onMouseDrag = function(event) {
-    path.add(event.point); //Add points to the path as the user drags their mouse.
-}
-
-$('#default').on('click', function (e) { //jquery click event code for our "pencil" button.
-    currentColor = 'black';  //change the color to black
-    currentWidth = 5;       //change the width to 5
-})
-$('#thick-green').on('click', function (e) { //jquery button click code for our "green paint" button.
-    currentColor = 'green';
-    currentWidth = 15;
-})
-$('#eraser').on('click', function (e) { //jquery button click code for our eraser button.
-    currentColor = 'white';
-    currentWidth = 20;
-})
+document.addEventListener("DOMContentLoaded", function () {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    let drawing = false;
+    let rotation = 0;
+    let scale = 1;
+  
+    const handlePanStart = () => {
+      drawing = true;
+    };
+  
+    const handlePanEnd = () => {
+      drawing = false;
+    };
+  
+    const handlePan = (event) => {
+      if (drawing) {
+        const x = event.changedPointers[0].clientX;
+        const y = event.changedPointers[0].clientY;
+  
+        // Your drawing logic here...
+        // For example, draw a small circle at the touch point
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, 2 * Math.PI);
+        ctx.fillStyle = "black";
+        ctx.fill();
+      }
+    };
+  
+    const handleRotate = () => {
+      rotation += 90;
+      canvas.style.transform = `rotate(${rotation}deg) scale(${scale})`;
+    };
+  
+    const handlePinch = (event) => {
+      scale = event.scale;
+      canvas.style.transform = `rotate(${rotation}deg) scale(${scale})`;
+    };
+  
+    const handleTap = () => {
+      // Handle tap event here
+      // For example, show a message when tapped
+      alert("Image tapped!");
+    };
+  
+    // Add Hammer.js listeners
+    const mc = new Hammer(canvas, {});
+    mc.on("panstart", handlePanStart);
+    mc.on("panend", handlePanEnd);
+    mc.on("pan", handlePan);
+    mc.on("rotate", handleRotate);
+    mc.on("pinch", handlePinch);
+    mc.on("tap", handleTap);
+  });
+  
+  function showPicture() {
+    // Implement the logic to show the picture here
+    alert("Show Picture");
+  }
+  
+  function rotatePicture() {
+    // Implement the logic to rotate the picture here
+    alert("Rotate Picture");
+  }
+  
+  function pinchPicture() {
+    // Implement the logic to pinch the picture here
+    alert("Pinch Picture");
+  }
+  
+  function tapPicture() {
+    // Implement the logic to handle tap on the picture here
+    alert("Tap Picture");
+  }
+  
